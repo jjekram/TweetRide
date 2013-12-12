ce5e21159e3b785bb8421039d25cd1dcc82c0094
Jal Jalali Ekram <jjekram@gmail.com>
2013-12-11 23:01:40 -0500

    Initial Commit

---
+++ TweetRide/TRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TRAppDelegate.h
+//  TweetRide
+//
+//  Created by Jal Jalali Ekram on 12/11/2013.
+//  Copyright (c) 2013 Jal Jalali Ekram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TweetRide/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TweetRide
+//
+//  Created by Jal Jalali Ekram on 12/11/2013.
+//  Copyright (c) 2013 Jal Jalali Ekram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TRAppDelegate class]));
+    }
+}
